991f0bd9181803d8725e63940ee23cdf886c4d6a
Chromatk <harrykwon20@gmail.com>
2015-09-20 02:20:16 -0500

    .

--- submitWriting.php
+++ submitWriting.php
@@ -2,6 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = file_get_contents("SQLPwKey.txt");
+$password = substr($password, 0, strlen($password)-1);
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
