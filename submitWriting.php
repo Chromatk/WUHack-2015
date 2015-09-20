0efb6d5f56302513c281145945b5d99ec806d11c
Chromatk <harrykwon20@gmail.com>
2015-09-20 01:45:44 -0500

    .

--- submitWriting.php
+++ submitWriting.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "harry ";
-$password = "rnjstkddbs";
+$username = "root";
+$password = "helenkwon9";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
