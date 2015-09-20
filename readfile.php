3cd3d40338a97f0a93848de21a630e49fee259fd
Chromatk <harrykwon20@gmail.com>
2015-09-20 10:32:57 -0500

    .

--- readfile.php
+++ readfile.php
@@ -3,5 +3,5 @@
     $hash = "07b7a9ede1f0c08e3b8a82e67b56215d";//$_POST["hash"];
     $text = file_get_contents($dir."/".$hash);
 
-    echo json_encode(array($text));
+    echo json_encode($text);
 ?>
