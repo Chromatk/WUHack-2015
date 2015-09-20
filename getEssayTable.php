3006e4bae63f3ffa66c4368ddb6b26fe1fb9799b
Chromatk <harrykwon20@gmail.com>
2015-09-20 05:23:27 -0500

    Revise bunch

---
+++ getEssayTable.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = file_get_contents("SQLPwKey.txt");
+$password = substr($password, 0, strlen($password)-1);
+$dbname = "essaysDB";
+
+//create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//Check connection
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error($con));
+}
+
+//select data
+$sql = 'Select * from Essays';
+$text = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+echo $text;
+
+?>
