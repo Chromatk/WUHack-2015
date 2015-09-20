44cf83937e447a6446925a0c3c1aa2c2fedb25da
Chromatk <harrykwon20@gmail.com>
2015-09-20 10:37:45 -0500

    .

--- readfile.php
+++ readfile.php
@@ -1,6 +1,6 @@
 <?php
     $dir = "WritingsArchive";
-    $hash = "07b7a9ede1f0c08e3b8a82e67b56215d";//$_POST["hash"];
+    $hash = $_POST["hash"];
     $text = file_get_contents($dir."/".$hash);
 
     echo $text;
