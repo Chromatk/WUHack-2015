6430f28b43f02ca7b3a531b5137b08478cd966dd
Chromatk <harrykwon20@gmail.com>
2015-09-19 23:08:24 -0500

    .

--- formSave.php
+++ formSave.php
@@ -1,10 +1,9 @@
 <?php
 $txt = $_POST["text"];
-<<<<<<< HEAD:WUHack2015/formSave.php
+
 $file = "formLog.txt";
-=======
 $file = "form.txt";
-<<<<<<< HEAD
+
 echo $txt;
 echo "\n";
 
@@ -12,14 +11,3 @@ file_put_contents($file, $txt);
 
 echo file_get_contents($file);
 ?>
-=======
->>>>>>> origin/master:WUHack2015/writeForm.php
-
-$current = file_get_contents($file);
-$current .= $txt;
-
-file_put_contents($file, $current);
-
-echo $file_get_contents($file);
-?>
->>>>>>> e49ac8ef708aabe3ed8b703f51dcd9d9e42e7753
