a9d19a453ed5aa0994d80cc1f9f22a490d73404c
Chromatk <harrykwon20@gmail.com>
2015-09-19 23:36:13 -0500

    .

--- formSave.php
+++ formSave.php
@@ -1,22 +1,13 @@
 <?php
-$dir = "log";
 
-if(!file_exists($dir)) {
-    echo "hi";
-    mkdir ($dir, 0777);
-}
-
-if(file_exists($dir)) {
-    echo "it's here";
-}
-
-$filePath = $dir."/formLog.txt";
+$file = "formLog.txt";
 $txt = $_POST["text"];
+$content = file_get_contents($file) . $txt;
 
 echo $txt;
 echo "\n";
 
-file_put_contents($filePath, $txt);
+file_put_contents($file, $content);
 
-echo file_get_contents($filePath);
+echo file_get_contents($file);
 ?>
